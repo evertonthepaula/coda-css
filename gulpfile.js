9b73f9fa3a7fb1f006fd79458e38feb1956d10f8
Everton de Paula <you@example.com>
2019-08-05 11:47:47 -0300

    Adicionar carregamento de fonts local

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,26 @@ const gulp = require('gulp'),
   imagemin = require('gulp-imagemin'),
   sourcemaps = require('gulp-sourcemaps'),
   gulpStylelint = require('gulp-stylelint'),
-  autoprefixer = require('gulp-autoprefixer');
+  autoprefixer = require('gulp-autoprefixer'),
+  googleWebFonts = require('gulp-google-webfonts'),
+  stripCssComments = require('gulp-strip-css-comments');
 
-const PATH_SRC = './src/';
-const PATH_DIST = './dist/';
+const SASS_PATH_SRC = './src/sass/';
+const SASS_PATH_DIST = './dist/';
 
-gulp.task('default', ['clean', 'lint-css', 'sass'], function () {
-
-});
+const FONTS_PATH_SRC = './src/fonts/';
+const FONTS_PATH_DIST = './dist/';
 
 gulp.task('clean', function () {
-  gulp.src(PATH_DIST + '**/*', { read: false })
+  gulp.src(SASS_PATH_DIST + '**/*', { read: false })
     .pipe(clean());
 });
 
+gulp.task('default', ['lint-css', 'sass'], function () {});
+
 gulp.task('lint-css', function () {
   gulp
-    .src(PATH_SRC + '/sass/**/*.scss')
+    .src(SASS_PATH_SRC + '**/*.scss')
     .pipe(gulpStylelint({
       fix: true,
       reporters: [
@@ -35,7 +38,7 @@ gulp.task('lint-css', function () {
 });
 
 gulp.task('sass', () => {
-  gulp.src(PATH_SRC + '/sass/index.scss')
+  gulp.src(SASS_PATH_SRC + 'index.scss')
     .pipe(sourcemaps.init())
     .pipe(
       autoprefixer({
@@ -51,11 +54,11 @@ gulp.task('sass', () => {
     )
     .pipe(sourcemaps.write())
     .pipe(concat('main.css'))
-    .pipe(gulp.dest(PATH_DIST + '/css'));
+    .pipe(gulp.dest(SASS_PATH_DIST + '/css'));
 });
 
 gulp.task('sass-min', () => {
-  gulp.src(PATH_SRC + '/sass/index.scss')
+  gulp.src(SASS_PATH_SRC + '/index.scss')
     .pipe(
       autoprefixer({
         browsers: ['last 2 versions'],
@@ -69,15 +72,27 @@ gulp.task('sass-min', () => {
         .on('error', sass.logError)
     )
     .pipe(concat('main.min.css'))
-    .pipe(gulp.dest(PATH_DIST + '/css'));
+    .pipe(stripCssComments())
+    .pipe(gulp.dest(SASS_PATH_DIST + '/css'));
 });
 
 gulp.task('sass:watch', ['clean', 'sass', 'images'], function () {
-  gulp.watch(PATH_SRC + '/sass/**/*.scss', ['sass']);
+  gulp.watch(SASS_PATH_SRC + '/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('fonts', function () {
+  gulp.src(FONTS_PATH_SRC + 'config/*.list')
+    .pipe(googleWebFonts({
+      fontsDir: 'fonts/',
+      cssDir: 'css/',
+      cssFilename: 'fonts.css'
+    }))
+    .pipe(gulp.dest(FONTS_PATH_DIST));
 });
 
 gulp.task('images', () => {
-  gulp.src(PATH_SRC + '/img/**/*')
+  gulp.src(SASS_PATH_SRC + '/img/**/*')
     .pipe(imagemin({ verbose: true }))
-    .pipe(gulp.dest(PATH_DIST + '/img/'))
+    .pipe(gulp.dest(SASS_PATH_DIST + '/img/'))
 });
+
